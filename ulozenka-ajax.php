8923247acf95fd5e3fb177e13dd2cc221d364a90
Václav Mach <info@prestahost.cz>
2014-12-06 00:33:41 +0100

    zapomenutý soubor

--- ulozenka-ajax.php
+++ ulozenka-ajax.php
@@ -1,44 +1,50 @@
 <?php
+ include(dirname(__FILE__).'/../../config/config.inc.php');
+ include(dirname(__FILE__).'/../../init.php');
+      $val  =Tools::getValue('selpobocka');
+       $retval['refresh'] =1;  // OPC=1 else 0
+      if(strlen($val) && strlen(Context::getContext()->cookie->ulozenka)) {
+             $retval['refresh'] =0;
+      }
+       $instance=Module::getInstanceByName('ulozenka');
+      Context::getContext()->cookie->ulozenka=$val;
+      $ceny=json_decode(Configuration::get('ULOZENKA_POBOCKY'), true);
+       if(isset($ceny[$val]) && strlen($ceny[$val])) {
+        $retval['cena'] = Tools::displayPrice(Tools::convertPrice($instance->addTax($ceny[$val])));
+     } 
+     
+      if($retval['cena'] == '') 
+           $retval['cena'] = Tools::displayPrice(Tools::convertPrice($instance->addTax($instance->getDefaultPrice())));
+    
+     $retval['cena'].=' ('.$instance->l('s DPH').')';
+           
 
-include(dirname(__FILE__) . '/../../config/config.inc.php');
-include(dirname(__FILE__) . '/../../init.php');
-$val = Tools::getValue('selpobocka');
-$retval['refresh'] = 1;  // OPC=1 else 0
-if (strlen($val) && strlen(Context::getContext()->cookie->ulozenka)) {
-    $retval['refresh'] = 0;
-}
-$instance = Module::getInstanceByName('ulozenka');
-Context::getContext()->cookie->ulozenka = $val;
-$ceny = json_decode(Configuration::get('ULOZENKA_POBOCKY'), true);
-if (isset($ceny[$val]) && strlen($ceny[$val])) {
-    $retval['cena'] = Tools::displayPrice(Tools::convertPrice($instance->addTax($ceny[$val])));
-}
+      // if($instance::isFreeShipping(Context::getContext()->cart->getOrderTotal(true, Cart::ONLY_PRODUCTS))) 
+       if(call_user_func(array($instance, 'isFreeShipping'), Context::getContext()->cart->getOrderTotal(true, Cart::ONLY_PRODUCTS)))    
+        $retval['cena'] =$instance->l('Zdarma');
+     
+     
+      
+      $retval['platba'] = $instance->ajax_getPaymentMethods(); 
+     if($val && strlen($val)) {
+          $retval['allow'] =1;
+     }
+     else {
+             $retval['allow'] =0;
+     }
+     
+      
+     
+     $retval['opc'] = (int)Configuration::get('PS_ORDER_PROCESS_TYPE'); 
+     if(Module::isEnabled('onepagecheckout'))
+       $retval['onepageactive'] =1;
+     else
+       $retval['onepageactive'] =0;
+     
 
-if ($retval['cena'] == '')
-    $retval['cena'] = Tools::displayPrice(Tools::convertPrice($instance->addTax($instance->getDefaultPrice())));
+     if(call_user_func(array($instance, 'getVersion')))
+     die(json_encode($retval));
+     
+     
 
-$retval['cena'].=' (' . $instance->l('s DPH') . ')';
-
-
-// if($instance::isFreeShipping(Context::getContext()->cart->getOrderTotal(true, Cart::ONLY_PRODUCTS))) 
-if (call_user_func(array($instance, 'isFreeShipping'), Context::getContext()->cart->getOrderTotal(true, Cart::ONLY_PRODUCTS)))
-    $retval['cena'] = $instance->l('Zdarma');
-
-
-
-$retval['platba'] = $instance->ajax_getPaymentMethods();
-if ($val && strlen($val)) {
-    $retval['allow'] = 1;
-} else {
-    $retval['allow'] = 0;
-}
-
-
-
-$retval['opc'] = (int) Configuration::get('PS_ORDER_PROCESS_TYPE');
-
-
-// $retval['version'] = $instance::getVersion();
-if (call_user_func(array($instance, 'getVersion')))
-    die(json_encode($retval));
 ?>
